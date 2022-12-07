44b1dbb3e7fda838368c6f2bd209b9fffa8facdf
João Pedro <112526011+JoaoPedro963@users.noreply.github.com>
2022-12-06 21:20:56 -0300

    Adição de comentários

--- app.js
+++ app.js
@@ -11,10 +11,11 @@ function tabuada(){
 let cont = 0
 let n1 = document.getElementById("numero").value
 document.getElementById("resultado").innerHTML = ""
+// o item a cima é utilizado para limpar a tela do resultado anterior
 while (cont <= 10){
     document.getElementById("resultado").innerHTML += (n1 + " X " + cont + " = " + cont * n1 + "<br>")
     cont++
-    
+    // o + após o "innerHTML" faz com que os itens dentro do laço continuem na tela, no caso não substituindo o item anterior
 }
 
 }
